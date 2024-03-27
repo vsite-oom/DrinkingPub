c4579900b54d2271a24c59c18b32412ba0f21f58
Julijan Šribar <julijan.sribar@gmail.com>
2024-03-27 08:28:25 +0100

    Add project files.

---
+++ DrinkingPub/PubBusiness.cs
@@ -0,0 +1,39 @@
+﻿namespace Vsite.Oom.DrinkingPub
+{
+    internal class PubBusiness
+    {
+        static void Main(string[] args)
+        {
+            // Cjenik artikala
+            Pricelist pricelist = new Pricelist("Kod veselog brace");
+            // Stavka ima naziv i cijenu
+            pricelist.AddItem("Uštrcak, 0.5 l", 1.23);
+            pricelist.AddItem("Coca Cola, 0.33 l", 1.5);
+            pricelist.AddItem("Ledeni čaj, 0.25 l", 1.75);
+            pricelist.AddItem("Vinjak Cezar, 0.02 l", 2.23);
+
+            // Svaki stol ima svoj broj i ime konobara koji ga poslužuje
+            Table table1 = new Table(1, "Andrej");
+            Table table2 = new Table(2, "Zoran");
+
+            // UKreira se narudžba...
+            Order order1 = new Order();
+            // ... i u nju se dodaju napitci i količina
+            order1.AddItem("Coca Cola, 0.33 l", 3);
+            order1.AddItem("Uštrcak, 0.5 l", 1);
+
+            // Zaprima se narudžba za određeni stol
+            table1.TakeOrder(order1);
+
+            Order order2 = new Order();
+            order2.AddItem("Vinjak Cezar, 0.02 l", 5);
+            table1.TakeOrder(order2);
+
+            // Na kraju se izračuna ukupni iznos računa za stol
+            double ceh1 = table1.TotalToPay(table1);
+            Console.WriteLine($"Račun za table1: {ceh1}");
+            double ceh2 = table2.TotalToPay(table1);
+            Console.WriteLine($"Račun za table2: {ceh2}");
+        }
+    }
+}
