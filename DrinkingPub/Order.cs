689039d48cab34ea1067a043d11686b30ef08a5e
Sonja Perković <soperkov@vsite.hr>
2024-03-28 21:12:34 +0100

    Implemented classes needed for program to work correctly.

---
+++ DrinkingPub/Order.cs
@@ -0,0 +1,21 @@
+﻿namespace Vsite.Oom.DrinkingPub
+{
+    internal class Order
+    {
+        public Order()
+        {
+        }
+
+        public void AddItem(string item, double howMuch)
+        {
+            Items.Add(new Tuple<string, double>(item, howMuch));
+        }
+
+        public double GetTotalPrice(Pricelist pricelist)
+        {
+            return Items.Sum(item => pricelist.GetItemPrice(item.Item1) * item.Item2);
+        }
+
+        private List<Tuple<string, double>> Items = new();
+    }
+}
