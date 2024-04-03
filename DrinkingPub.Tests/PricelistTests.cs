97a675a0297b382f5e59d179beeffdb6f688e59f
AnMal <junkie_@msn.com>
2024-04-03 11:27:07 +0200

    Add unit tests

---
+++ DrinkingPub.Tests/PricelistTests.cs
@@ -0,0 +1,45 @@
+﻿using Vsite.Oom.DrinkingPub;
+
+namespace DrinkingPub.Tests
+{
+    [TestClass]
+    public class PricelistTests
+    {
+        [TestMethod]
+        public void Constructor_WithEmptyName_ThrowsError()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new Pricelist(""));
+        }
+
+        [TestMethod]
+        public void AddItem_WithInvalidName_ThrowsError()
+        {
+            Pricelist pricelist = new("Birtija");
+            var itemPrice = 1.0;
+
+            Assert.ThrowsException<ArgumentException>(() => pricelist.AddItem(null, itemPrice));
+        }
+
+        [TestMethod]
+        public void AddItem_WithEmptyName_ThrowsError()
+        {
+            Pricelist pricelist = new("Birtija");
+            var itemName = "";
+            var itemPrice = 1.0;
+
+            Assert.ThrowsException<ArgumentException>(() => pricelist.AddItem(itemName, itemPrice));
+        }
+
+        [TestMethod]
+        public void AddItem_WithValidNameAndPrice_AddsItemToPricelist()
+        {
+            Pricelist pricelist = new("Birtija");
+            var itemName = "Beer";
+            var itemPrice = 1.0;
+
+            pricelist.AddItem(itemName, itemPrice);
+
+            Assert.AreEqual(itemPrice, pricelist.Items[itemName]);
+        }
+    }
+}
