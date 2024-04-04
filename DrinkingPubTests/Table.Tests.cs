b70769dfd5e4370b314537fdd2d4931393a9c76a
Benjamin Gajski <benjamin.gajski@gmail.com>
2024-04-04 15:32:14 +0200

    kreiranje klase pricelisttests

---
+++ DrinkingPubTests/Table.Tests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Vsite.Oom.DrinkingPub.Tests
+{
+    [TestClass]
+    public class Table
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+    }
+}
