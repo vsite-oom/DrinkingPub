9fea16b8f9d5f6f2fa6b637072686fc100f21e0f
Benjamin Gajski <benjamin.gajski@gmail.com>
2024-04-04 16:03:55 +0200

    kreiranje klase tabletests i oredertests

---
+++ DrinkingPubTests/OrderTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Vsite.Oom.DrinkingPub.Tests
+{
+    [TestClass]
+    public class OrderTests
+    {
+        [TestMethod]
+        public void AddItem_WithValidData_AddsItemCorrectly()
+
+        {
+            Order order = new Order();
+            string itemName = "testna kava";
+            int itemQuantity = 2;
+
+            order.AddItem(itemName, itemQuantity);
+
+            Assert.AreEqual(itemQuantity, order.Items[itemName]);
+            Assert.IsTrue(order.Items.ContainsKey(itemName));
+        }
+
+        [TestMethod]
+        public void AddItem_WithNullItem_ThrowsArgumentException()
+        {
+            Order order = new Order();
+
+            Assert.ThrowsException<ArgumentException>(() => order.AddItem(null, 1));
+        }
+
+        [TestMethod]
+        public void AddItem_EmptyItemName_ThrowsArgumentException()
+        {
+            Order order = new Order();
+
+            Assert.ThrowsException<ArgumentException>(() => order.AddItem("testna kava", 0));
+        }
+    }
+}
--- DrinkingPubTests/Table.Tests.cs
+++ DrinkingPubTests/Table.Tests.cs
@@ -4,12 +4,35 @@ using System;
 namespace Vsite.Oom.DrinkingPub.Tests
 {
     [TestClass]
-    public class Table
+    public class TableTests
     {
         [TestMethod]
-        public void MyTestMethod()
+        public void TakeOrder_AddsOrderToTable()
         {
+            Table table = new Table(1, "test table");
+            Order order = new Order();
+            order.AddItem("testna kava", 2);
 
+            table.TakeOrder(order);
+        }
+
+        [TestMethod]
+        public void TotalToPay_WithSingleOrders_CalculatesTotalCorrectly()
+        {
+            Pricelist pricelist = new Pricelist("testni pub");
+            pricelist.AddItem("testna kava", 0.60);
+            pricelist.AddItem("testna piva", 1.25);
+
+            Table table = new Table(1, "testni stol");
+            Order order = new Order();
+            order.AddItem("testna kava", 3);
+            order.AddItem("testna piva", 3);
+            
+            table.TakeOrder(order);
+            double total = table.TotalToPay(pricelist);
+            double expectedTotal = 3 * 0.60 + 3 * 1.25;
+
+            Assert.AreEqual(expectedTotal, total); 
         }
     }
 }
