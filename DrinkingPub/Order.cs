e63f8f6c7420c1ccf555032375fe848dbb7122d0
Marko Pavić <mapavic@vsite.hr>
2024-04-10 11:41:42 +0200

    MPavic Pub

---
+++ DrinkingPub/Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.DrinkingPub
+{
+	public class Order
+	{
+		private readonly Dictionary<string, int> items;
+
+		public Order()
+		{
+			items = new Dictionary<string, int>();
+		}
+
+		public void AddItem(string name, int value)
+		{
+			if(items.ContainsKey(name))
+			{
+				items[name] += value;
+			}
+			else
+			{
+				items[name]=value;
+			}
+		}
+		public Dictionary<string, int> Items => items;
+	}
+}
---
+++ DrinkingPub/Table.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.DrinkingPub
+{
+	public class Table
+	{
+		private readonly int broj;
+		private readonly string konobar;
+		private Order currentOrder;
+
+		public Table(int broj, string konobar)
+		{
+			this.broj = broj;
+			this.konobar = konobar;
+		}
+		public void TakeOrder(Order order)
+		{
+			if (currentOrder == null)
+			{
+				currentOrder = order;
+			}
+			else
+			{
+				foreach (var item in order.Items)
+				{
+					if(currentOrder.Items.ContainsKey(item.Key))
+					{
+						currentOrder.Items[item.Key] += item.Value;
+					}
+					else
+					{
+						currentOrder.Items[item.Key ] = item.Value;
+					}
+				}
+			}
+		}
+		public double TotalToPay(Pricelist pricelist)
+		{
+			double total = 0;
+			if (currentOrder == null)
+			{
+				total = 0;
+			}
+			else
+			{
+				foreach (var item in currentOrder.Items)
+				{
+					double price = pricelist.GetPrice(item.Key);
+					total += price * item.Value;
+				}
+			}
+			total = Math.Round(total, 2);
+			return total;
+		}
+	}
+}
