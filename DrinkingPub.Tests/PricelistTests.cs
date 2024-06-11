08c538479846d43ebea634bd5750f8f8332babb8
Đuro Rukavina <djrukavina@vsite.hr>
2024-06-11 11:00:33 +0200

    Added Test project with Test classes for each class

---
+++ DrinkingPub.Tests/PricelistTests.cs
@@ -0,0 +1,54 @@
+﻿using Vsite.Oom.DrinkingPub;
+
+namespace DrinkingPub.Tests
+{
+    [TestClass]
+    public class PricelistTests
+    {
+        [TestMethod]
+        public void CreatePricelistWithNameProvided()
+        {
+            string ime = "Kafana";
+            Pricelist cjenik = new Pricelist(ime);
+            Assert.IsTrue(cjenik.pubName == ime);
+        }
+        [TestMethod]
+        public void CreatePricelistWithoutNameProvided()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new Pricelist(""));
+        }
+        [TestMethod]
+        public void AddItemWithNameAndPriceProvided()
+        {
+            string imePuba = "Kafana";
+            Pricelist cjenik = new Pricelist(imePuba);
+            string imeArtikla = "Kokta";
+            double cijenaArtikla = 1.5;
+            cjenik.AddItem(imeArtikla, cijenaArtikla);
+            Assert.AreEqual(cjenik.GetPrice(imeArtikla), cijenaArtikla);
+        }
+        [TestMethod]
+        public void AddItemWithoutNameProvided()
+        {
+            string imePuba = "Kafana";
+            Pricelist cjenik = new Pricelist(imePuba);
+            string? imeArtikla = null;
+            double cijenaArtikla = 1.5;
+            Assert.ThrowsException<ArgumentException>(() => cjenik.AddItem(imeArtikla!, cijenaArtikla));
+        }
+        [TestMethod]
+        public void GetItemNotOnPricelist()
+        {
+            string imePuba = "Kafana";
+            Pricelist cjenik = new Pricelist(imePuba);
+            Assert.ThrowsException<ArgumentException>(() => cjenik.GetPrice("Rakija"));
+        }
+        [TestMethod]
+        public void GetNullItemThrowsException()
+        {
+            string imePuba = "Kafana";
+            Pricelist cjenik = new Pricelist(imePuba);
+            Assert.ThrowsException<ArgumentException>(() => cjenik.GetPrice(null!));
+        }
+    }
+}
---
+++ DrinkingPub.Tests/TableTests.cs
@@ -0,0 +1,77 @@
+﻿using Vsite.Oom.DrinkingPub;
+
+namespace DrinkingPub.Tests
+{
+    [TestClass]
+    public class TableTests
+    {
+        [TestMethod]
+        public void CreateTableWithTablenumberAndWaiternameProvided()
+        {
+            int brojStola = 1;
+            string imeKonobara = "Šime";
+            var stol = new Table(brojStola, imeKonobara);
+            Assert.AreEqual(brojStola, stol.table_number);
+            Assert.AreEqual(imeKonobara, stol.waiter_name);
+        }
+        [TestMethod]
+        public void CreateTableWithoutTablenumberProvided()
+        {
+            int brojStola = 0;
+            string imeKonobara = "Šime";
+            Assert.ThrowsException<ArgumentException>(() => new Table(brojStola, imeKonobara));
+        }
+        [TestMethod]
+        public void CreateTableWithoutWaiternameProvided()
+        {
+            int brojStola = 1;
+            Assert.ThrowsException<ArgumentException>(() => new Table(brojStola, null!));
+        }
+        [TestMethod]
+        public void TakeOrderIsNullThrowsError()
+        {
+            int brojStola = 1;
+            string imeKonobara = "Šime";
+            var stol = new Table(brojStola, imeKonobara);
+            Assert.ThrowsException<ArgumentNullException>(() => stol.TakeOrder(null!));
+        }
+        [TestMethod]
+        public void TotalToPayPricelistiIsNullThrowsError()
+        {
+            int brojStola = 1;
+            string imeKonobara = "Šime";
+            var stol = new Table(brojStola, imeKonobara);
+            Assert.ThrowsException<ArgumentNullException>(() => stol.TotalToPay(null!));
+        }
+        [TestMethod]
+        public void TotalToPayWith1ItemReturnsPrice()
+        {
+            var cjenik = new Pricelist("Kafana");
+            cjenik.AddItem("Brlja", 0.75);
+            var stol = new Table(1, "Mirko");
+            var narudzba = new Order();
+            narudzba.AddItem("Brlja", 1);
+            stol.TakeOrder(narudzba);
+
+            Assert.AreEqual(0.75, stol.TotalToPay(cjenik));
+        }
+        [TestMethod]
+        public void TotalToPayWithMultipleItemReturnsPrice()
+        {
+            var cjenik = new Pricelist("Kafana");
+            cjenik.AddItem("Brlja", 0.75);
+            cjenik.AddItem("Viski", 2.25);
+            cjenik.AddItem("Pivo", 1.10);
+            var stol = new Table(1, "Mirko");
+            var narudzba = new Order();
+            narudzba.AddItem("Brlja", 2);
+            narudzba.AddItem("Viski", 1);
+            narudzba.AddItem("Brlja", 2);
+            narudzba.AddItem("Pivo", 3);
+            narudzba.AddItem("Pivo", 3);
+            stol.TakeOrder(narudzba);
+
+            Assert.AreEqual(11.85, stol.TotalToPay(cjenik));
+        }
+    }
+}
